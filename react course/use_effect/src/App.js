116b3f9067fd1a57a1f005292f94298b4126b184
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-01-29 22:39:29 +0530

    timer app created

--- react course/use_effect/src/App.js
+++ react course/use_effect/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Timer1 from './components/Timer1';
 import Timer from './components/Timer';
 function App() {
   return (
     <div className="App">
       <Timer/>
+      <Timer1/>
     </div>
   );
 }
